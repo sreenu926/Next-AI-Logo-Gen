486a24a9f5e39362761c86d9fe735ebf0ac13e11
GreatStackDev <your-email@example.com>
2025-03-19 13:50:39 +0530

    Landing page modified

--- app/_components/Header.jsx
+++ app/_components/Header.jsx
@@ -14,7 +14,7 @@ function Header() {
   const [menuOpen, setMenuOpen] = useState(false);
 
   return (
-    <div className="px-10 lg:px-32 xl:px-48 2xl:px-56 p-4 flex justify-between items-center shadow-sm">
+    <div className="px-6 lg:px-32 xl:px-48 2xl:px-56 p-2 md:p-4 flex justify-between items-center shadow-sm">
       {/* Logo */}
       <Link href={"/"}>
         <Image
--- app/_components/Hero.jsx
+++ app/_components/Hero.jsx
@@ -3,34 +3,43 @@ import React, { useState } from "react";
 import Lookup from "../_data/Lookup";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
+import Image from "next/image";
 
 function Hero() {
   const [logoTitle, setLogoTitle] = useState();
   return (
-    <div className="flex items-center mt-16 md:mt-32 flex-col gap-15">
-      <h2 className="text-pink-600 text-4xl md:text-6xl font-bold">
+    <div className="flex items-center mt-6 md:mt-14 flex-col gap-4 md:gap-10">
+      <h2 className="text-pink-600 text-3xl md:text-6xl font-bold">
         {Lookup.HeroHeading}
       </h2>
-      <h2 className="text-2xl text-center md:text-4xl font-bold">
+      <h2 className="text-xl text-center md:text-4xl font-bold">
         {Lookup.HeroSubheading}
       </h2>
-      <p className="text-sm md:text-lg text-center text-gray-500">
+      <p className="text-xs md:text-lg text-center text-gray-500">
         {Lookup.HeroDesc}
       </p>
 
       <div className="flex gap-6 w-full justify-center max-w-2xl">
-        {/* <input
-          className="p-3 border rounded-md w-3/4 shadow-md"
-          type="text"
-          placeholder={Lookup.InputTitlePlaceholder}
-          onChange={(event) => setLogoTitle(event?.target.value)}
-        /> */}
         <Link
           className="border-2 border-white hover:border-black hover:rounded-sm"
           href={"/create?title=" + logoTitle}
         >
           <Button className="bg-red-500 p-6 ">Get Started</Button>
         </Link>
+      </div>
+      <div className="border-2 p-2 rounded-lg">
+        <p className="text-center mb-4 text-sm md:text-xl border p-1 rounded-lg bg-amber-900 text-white">
+          Preview of Premium Logos Generated.
+        </p>
+        <div className="w-[400] md:w-[800]">
+          <Image
+            src={"/landing.png"}
+            alt="landing"
+            width={800}
+            height={800}
+            unoptimized
+          />
+        </div>
       </div>
     </div>
   );
